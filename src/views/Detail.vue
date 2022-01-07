4f4873d3f8028e99e69688c78707e91086665f4f
Jenifer Prochnow <jeniferprochnow@web.de>
2022-01-07 22:15:45 +0100

    use api, clean code

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,48 +1,15 @@
 <template>
   <div class='detail'>
-
     <div class="teaser" style="position: relative">
-      <div class="coverphoto"
-      :style="
-      {
-        'background-image': 'url(' + space.coverphoto +  ')',
-        'max-height': '300px',
-        'height': '50vh',
-        'width': '100%',
-        'background-size': 'cover',
-        'background-repeat': 'no-repeat',
-        'margin-bottom': '75px',
-
-      }">
-
-      </div>
-      <div class="logo"
-      :style="
-      {
-        'background-image': 'url(' + space.logo +  ')',
-        'height': '150px',
-        'width': '150px',
-        'background-size': 'contain',
-        'background-repeat': 'no-repeat',
-        'position': 'absolute',
-        'bottom': '-75px',
-        'margin': 'auto',
-        'left': 0,
-        'right': 0,
-        'border-radius': '50%',
-        'border': '8px solid #fff',
-      }"
-      >
-      </div>
+      <div class="coverphoto" :style="{ 'background-image': 'url(' + space.coverPhoto +  ')' }"/>
+      <div class="logo" :style="{'background-image': 'url(' + space.logo +  ')'}"/>
     </div>
     <div class="space-info">
       <h1>{{ space.name }}</h1>
       <p>{{ space.description }}</p>
-      <p class="address">{{ space.map.address }}</p>
+      <p class="address">{{ space.map ? space.map.address : '' }}</p>
     </div>
-
     <a :href="space.website" class="button cta">Visit website</a>
-
 </div>
 </template>
 
@@ -63,61 +30,49 @@
   p, a {
     font-size: 1.2rem;
   }
+
+  .coverphoto {
+    max-height: 300px;
+    height: 50vh;
+    width: 100%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    margin-bottom: 75px;
+  }
+
+  .logo {
+    height: 150px;
+    width: 150px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: absolute;
+    bottom: -75px;
+    margin: auto;
+    left: 0;
+    right: 0;
+    border-radius: 50%;
+    border: 8px solid #fff;
+    background-position: center;
+    -webkit-box-shadow: 0px 0px 10px 0px #333333;
+    box-shadow: 0px 5px 10px 0px #666;
+  }
 </style>
 
 <script>
+const axios = require('axios');
+
 export default {
   data() {
     return {
-      space: {
-        country: 'United Kingdom',
-        city: 'London',
-        map: {
-          address: '98 Victoria Road, London NW10 6NB, United Kingdom',
-          lat: '51.5255905',
-          lng: '-0.2549043999999867',
-        },
-        description: 'Space description: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-        email: 'space@email.com',
-        website: 'www.spacesite.com',
-        logo: './img/test_logo.png',
-        coverphoto: './img/test_teaser.jpeg',
-        name: 'Coworking Space Space Name',
-        slug: 'space-slug',
-      },
-      markers: [
-        {
-          city: 'London',
-          map: {
-            address: '43c Old London Road, Kingston upon Thames, United Kingdom',
-            lat: '51.41088689999999',
-            lng: '-0.29889170000001286',
-          },
-          name: 'Kingston CoWork',
-          slug: 'kingston-cowork',
-        },
-        {
-          city: 'London',
-          map: {
-            address: '98 Victoria Road, London NW10 6NB, United Kingdom',
-            lat: '51.5255905',
-            lng: '-0.2549043999999867',
-          },
-          name: 'Cowork Hub',
-          slug: 'cowork-hub',
-        },
-        {
-          city: 'Guildford',
-          map: {
-            address: '20 Nugent Road, Surrey Research Park, Guildford, Surrey, GU2 7AF',
-            lat: '51.24096669999999',
-            lng: '-0.6153684999999314',
-          },
-          name: 'Rocketdesk',
-          slug: 'rocketdesk',
-        },
-      ],
+      space: {},
     };
+  },
+  mounted() {
+    axios.get('https://coworking-explorer.jencoding.com/spaces/sleevesup-hannover')
+      .then((response) => {
+        this.space = response.data;
+        console.log(response);
+      });
   },
 };
 </script>
