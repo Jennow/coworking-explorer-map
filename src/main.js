952a7f22c96d1fa9be6192c0c08ad32a160859d3
Jennow <jeniferprochnow@web.de>
2022-01-10 09:27:13 +0100

    remove apiKey

--- src/main.js
+++ src/main.js
@@ -9,6 +9,6 @@ const app = createApp(App);
 app.use(VueGoogleMaps, {
   load: {
     map_id: '213208b604b031da',
-    key: 'AIzaSyDhxOwag-IF4_zG-en8gf0Mm1IUwan3LbE',
+    key: '{{apiKey}}',
   },
 }).use(router).mount('#app');
